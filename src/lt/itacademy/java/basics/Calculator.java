e1ac8815f2fc21df210117962ded038e6f8f03d4
Austėja Z <austeja.zasimauskaite@gmail.com>
2022-03-04 10:45:22 +0200

    Task 10 edited

--- src/lt/itacademy/java/basics/Calculator.java
+++ src/lt/itacademy/java/basics/Calculator.java
@@ -4,51 +4,60 @@ import java.util.Scanner;
 
 public class Calculator {
     public static void main(String[] args) {
-
+        boolean cont = true;
+        while (cont) {
         Scanner sc = new Scanner(System.in);
-
         System.out.println("MENU:");
         System.out.println("1. Perimeter and area of square");
         System.out.println("2. Perimeter and area of triangle");
         System.out.println("3. Perimeter and area of rectangle");
-        System.out.println("Please enter any of the above option: ");
+        System.out.println("Please enter any of the above option (1, 2, or 3): ");
 
-        int num = sc.nextInt();
-        switch(num) {
-           case 1:
-               System.out.println("Please enter edge of square: ");
-               double edge = sc.nextFloat();
-               double areaSquare = edge * edge;
-               double perimeterSquare = 4 * edge;
-               System.out.println("Square perimeter is " + perimeterSquare + " and areas is " + areaSquare);
-               break;
+            int num = sc.nextInt();
+            switch (num) {
+                case 1:
+                    System.out.println("Please enter edge of square: ");
+                    double edge = sc.nextFloat();
+                    double areaSquare = edge * edge;
+                    double perimeterSquare = 4 * edge;
+                    System.out.println("Square perimeter is " + perimeterSquare + " and areas is " + areaSquare);
+                    break;
+                case 2:
+                    System.out.println("Please enter edges, height and base of triangle: ");
+                    double height = sc.nextFloat();
+                    double base = sc.nextFloat();
+                    double triangleEdge1 = sc.nextFloat();
+                    double triangleEdge2 = sc.nextFloat();
+                    double triangleEdge3 = sc.nextFloat();
+                    double areaTriangle = (height * base) / 2;
+                    double perimeterTriangle = triangleEdge1 + triangleEdge2 + triangleEdge3;
+                    System.out.println("Triangle perimeter is " + perimeterTriangle + " and areas is " + areaTriangle);
+                    break;
+                case 3:
+                    System.out.println("Please enter edges of rectangle: ");
+                    double rectangleEdge1 = sc.nextFloat();
+                    double rectangleEdge2 = sc.nextFloat();
+                    double areaRectangle = rectangleEdge1 * rectangleEdge2;
+                    double perimeterRectangle = (rectangleEdge1 * 2) + (rectangleEdge2 * 2);
+                    System.out.println("Rectangle perimeter is " + perimeterRectangle + " and areas is " + areaRectangle);
+                    break;
 
-            case 2:
-                System.out.println("Please enter edges, height and base of triangle: ");
-                double height = sc.nextFloat();
-                double base = sc.nextFloat();
-                double triangleEdge1 = sc.nextFloat();
-                double triangleEdge2 = sc.nextFloat();
-                double triangleEdge3 = sc.nextFloat();
-                double areaTriangle = (height * base) / 2;
-                double perimeterTriangle = triangleEdge1 + triangleEdge2 + triangleEdge3;
-                System.out.println("Triangle perimeter is " + perimeterTriangle + " and areas is " + areaTriangle);
-                break;
+                default:
+                    System.out.println("Invalid operator!");
+                    break;
 
-            case 3:
-                System.out.println("Please enter edges of rectangle: ");
-                double rectangleEdge1 = sc.nextFloat();
-                double rectangleEdge2 = sc.nextFloat();
-                double areaRectangle = rectangleEdge1 * rectangleEdge2;
-                double perimeterRectangle = (rectangleEdge1 * 2) + (rectangleEdge2 * 2);
-                System.out.println("Rectangle perimeter is " + perimeterRectangle + " and areas is " + areaRectangle);
-                break;
-            default:
-                System.out.println("Invalid operator!");
+            }
+
+            System.out.println("Do you want to repeat the program? (y/n)");
+
+            String contin = sc.nextLine();
+            if (contin.equals("n") || contin.equals("N") || contin.equals("No") || contin.equals("NO") || contin.equals("no")) {
+                cont = false;
+            }
+
+
 
         }
-
-      sc.close();
     }
 }
 
